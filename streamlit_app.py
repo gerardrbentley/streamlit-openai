999f24b6cb0fd2429574b0df7b0f060cb031e040
Gerard Bentley <gerardrbentley@gmail.com>
2023-03-06 13:42:25 -0800

    help: show request code

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ if user_keys is None or not isinstance(user_keys, list):
     st.error("No user_keys array in .streamlit/secrets.toml")
     st.stop()
 
+st.header("Streamlit + OpenAI App 🎈🤖")
+
 with st.expander("What's this?"):
     st.write(
         """\
@@ -39,12 +41,6 @@ Made with ❤️ by [Gerard Bentley](https://gerardbentley.com)
 """
     )
     st.image("media/golang_demo.gif")
-
-query_params = st.experimental_get_query_params()
-query_user_key_values = query_params.get("user_key")
-if not query_user_key_values or not set(user_keys).intersection(query_user_key_values):
-    st.error("Invalid User. Must be Admin with Key to continue")
-    st.stop()
 
 number_of_responses = st.sidebar.slider(
     "Number of Responses per Prompt Submission",
@@ -83,9 +79,6 @@ with st.form("input"):
     chat_input = st.text_area("ChatGPT input:")
     is_submitted = st.form_submit_button()
 
-if not is_submitted or not chat_input:
-    st.warning("Enter text and hit Submit to continue!")
-    st.stop()
 
 request = ChatGPTRequest(
     messages=[
@@ -97,6 +90,28 @@ request = ChatGPTRequest(
     presence_penalty=presence_penalty,
     frequency_penalty=frequency_penalty,
 )
+
+with st.expander("Sample Python Code"):
+    formatted_kwargs = ',\n'.join((f'\t\t{key}={value!r}' for key, value in request.to_kwargs().items()))
+    st.code(f"""\
+import openai
+
+openai.api_key = load_your_api_key_string_here
+response = openai.ChatCompletion.create(
+{formatted_kwargs}
+chat_responses = response.get("choices")
+)
+""")
+
+query_params = st.experimental_get_query_params()
+query_user_key_values = query_params.get("user_key")
+if not query_user_key_values or not set(user_keys).intersection(query_user_key_values):
+    st.error("Invalid User. Must be Admin with Key to continue")
+    st.stop()
+
+if not is_submitted or not chat_input:
+    st.warning("Enter text and hit Submit to continue!")
+    st.stop()
 
 response = openai.ChatCompletion.create(**request.to_kwargs())
 
