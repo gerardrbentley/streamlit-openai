01062fc79980955629f919080c305d4dc85a9b2c
gerardrbentley <gerardrbentley@gmail.com>
2023-07-15 15:15:27 -0700

    feat: download w/ simpleaichat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import uuid
+
 import streamlit as st
 from st_paywall import add_auth
 from simpleaichat import AIChat
+import pandas as pd
 
 SESSION_ID = "session_id"
 DEFAULT_SYSTEM_PROMPT = "You are a helpful assistant."
@@ -12,10 +14,21 @@ st.set_page_config(
     page_icon="🕹",
     initial_sidebar_state="collapsed",
 )
-add_auth(required=False)
-
 st.header("Streamlit + OpenAI App 🎈🤖")
 
+add_auth(required=False)
+
+if not st.session_state.get('user_subscribed'):
+    st.warning("""\
+# Sign In On the Left
+
+Due to API costs associated with running this app, you must subscribe for a low cost.
+""")
+    st.stop()
+else:
+    st.info("""\
+Download this chat from the left sidebar
+""")
 
 @st.cache_resource
 def get_ai() -> AIChat:
@@ -67,3 +80,27 @@ with st.chat_message("assistant"):
         # Add a blinking cursor to simulate typing
         message_placeholder.markdown(full_response + "▌")
     message_placeholder.markdown(chunk["response"])
+
+df = pd.DataFrame([{'role': message.role, 'content': message.content}for message in ai_session.messages])
+with st.sidebar.expander('Raw Chat Data'):
+    st.sidebar.info("""Copy all entries to excel: 
+
+- Click one cell
+- Select all (`ctrl + a`)
+- Copy (`ctrl + c`)
+- Paste in Excel (`ctrl + v`)
+
+Macs use `cmd` instead of `ctrl` 
+""")
+    st.sidebar.dataframe(df)
+
+def prep_download(df: pd.DataFrame) -> bytes:
+    return df.to_csv(index=False).encode('utf-8')
+
+st.sidebar.download_button(
+    label="Download Conversation as CSV",
+    data=prep_download(df),
+    file_name='chatgpt_conversation.csv',
+    mime='text/csv',
+    help="This format of tab separated column values is suitable for copying into excel"
+)
